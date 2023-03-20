4b5554ec39ecd7adf2cbef1432f0ec6b05d22b7c
AlexNguyenSJSU <alexnguyen091203@gmail.com>
2023-03-20 15:30:44 -0700

    Adding file cacti.py after debugging

---
+++ cacti.py
@@ -0,0 +1,29 @@
+def cacti_number(grid):
+    result = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+
+            if grid[i][j] == 0:
+                check = True
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    check = False
+                if j > 0 and grid[i][j - 1] == 1:
+                    check = False
+                if i < len(grid) - 1 and grid[i + 1][j] == 1:
+                    check = False
+                if j < len(grid[0]) - 1 and grid[i][j + 1] == 1:
+                    check = False
+                if i < len(grid) - 1 and grid[i + 1][j] ==1:
+                    check = False
+                if j < len(grid[0]) - 1 and grid[i][j + 1] == 1:
+                    check = False
+
+            if check:
+                result += 1
+                grid[i][j] = 1
+
+    return result
