611616fb51e5fc4e3c84204334193a071bb9273b
Anh Nguyen <107016480+AlexNguyenSJSU@users.noreply.github.com>
2023-03-20 23:42:38 -0700

    Fixing 1 minor bug

--- cacti.py
+++ cacti.py
@@ -1,10 +1,8 @@
 def cacti_number(grid):
     result = 0
-    rows = len(grid)
-    cols = len(grid[0])
 
-    for i in range(rows):
-        for j in range(cols):
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
 
             if grid[i][j] == 0:
                 check = True
@@ -17,7 +15,7 @@ def cacti_number(grid):
                     check = False
                 if j < len(grid[0]) - 1 and grid[i][j + 1] == 1:
                     check = False
-                if i < len(grid) - 1 and grid[i + 1][j] ==1:
+                if i < len(grid) - 1 and grid[i + 1][j] == 1:
                     check = False
                 if j < len(grid[0]) - 1 and grid[i][j + 1] == 1:
                     check = False
